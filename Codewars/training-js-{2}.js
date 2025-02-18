b75551c9d174d8a588b9bf12781b8fbb6a5c8a33
Ashot <adodus13@gmail.com>
2025-02-18 21:06:20 +0300

    2

---
+++ Codewars/training-js-{2}.js
@@ -0,0 +1,32 @@
+v1=50
+v2=100
+v3=150
+v4=200
+v5=2
+v6=250
+
+equal1=() ->
+    a=v1
+b=v1
+return a+b
+
+#Please refer to the example above to complete the following functions
+equal2=()->
+    a=  v3 #set number value to a
+b=  v1#set number value to b
+return a-b
+
+equal3=()->
+    a=  v1 #set number value to a
+b=   v5#set number value to b
+return a*b
+
+equal4=()->
+    a=   v4#set number value to a
+b=   v5#set number value to b
+return a/b
+
+equal5=()->
+    a=   v6#set number value to a
+b=   v3#set number value to b
+return a%b
