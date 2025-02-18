e19f9e94946dfb24c4ab18db49dc0a977b366e6f
Ashot <adodus13@gmail.com>
2025-02-18 21:06:27 +0300

    4

---
+++ Codewars/training-js-{4}.js
@@ -0,0 +1,20 @@
+getLength = (arr) ->
+    arr.length
+
+
+getFirst = (arr) ->
+    arr[0]
+
+getLast = (arr) ->
+    arr[arr.length - 1]
+
+
+pushElement = (arr) ->
+    el = 1
+arr.push el
+arr
+
+
+popElement = (arr) ->
+    arr.pop()
+arr
