1d088b7476e9dda8143f158d3ee4151ee723e642
Ashot <adodus13@gmail.com>
2025-02-18 21:06:24 +0300

    3

---
+++ Codewars/training-js-{3}.js
@@ -0,0 +1,23 @@
+a1='A'
+a2='a'
+b1='B'
+b2='b'
+c1='C'
+c2='c'
+d1='D'
+d2='d'
+e1='E'
+e2='e'
+n1='N'
+n2='n'
+Dad=()->
+    #select some variable to combine 'Dad'
+d1+a2+d2?
+
+    Bee=()->
+        #select some variable to combine 'Bee'
+b1+e2+e2
+
+banana=()->
+    #select some variable to combine 'banana'
+b2+a2+n2+a2+n2+a2
